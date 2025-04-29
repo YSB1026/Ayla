4d2985932b2ee675dcecead15e967e7aefb49dc5
gwon716 <nowmyturn716@gmail.com>
2025-04-29 09:44:33 +0900

    update player
    add : player dead animation, enemy_light script

---
+++ Assets/Personal/LJW/Script/Enemy/LightEnemy/Enemy_Light.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Enemy_Light : Enemy
+{
+    #region States
+    #endregion
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    /*public override bool Stop()
+    {
+        // ∫“∫˚¿Ã ¿÷¿ª ∂ß ∏ÿ√ﬂ±‚
+        return true;
+    }*/
+}
---
+++ Assets/Personal/LJW/Script/Enemy/LightEnemy/Enemy_Light_MoveState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Enemy_Light_MoveState : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
