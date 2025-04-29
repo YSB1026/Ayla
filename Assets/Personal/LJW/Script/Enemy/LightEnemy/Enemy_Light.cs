7861dded18ae10452dc31093151c542919b6c08f
gwon716 <nowmyturn716@gmail.com>
2025-04-29 18:00:56 +0900

    test enemy
    enemy light

--- Assets/Personal/LJW/Script/Enemy/LightEnemy/Enemy_Light.cs
+++ Assets/Personal/LJW/Script/Enemy/LightEnemy/Enemy_Light.cs
@@ -2,26 +2,44 @@ using UnityEngine;
 
 public class Enemy_Light : Enemy
 {
+    [Header("추가 정보")]
+    public Transform player;
+    public LayerMask lightLayer;
+    public float detectRadius = 0.5f;
+
+    public bool isInLight { get; private set; }
+
     #region States
+    public Enemy_Light_MoveState moveState { get; private set; }
     #endregion
     protected override void Awake()
     {
         base.Awake();
+
+        moveState = new Enemy_Light_MoveState(this, stateMachine, "Move");
     }
 
     protected override void Start()
     {
         base.Start();
+
+        stateMachine.Initialize(moveState);
     }
 
     protected override void Update()
     {
         base.Update();
+
+        stateMachine.currentState.Update();
     }
 
-    /*public override bool Stop()
+    public void SetInLight(bool inLight)
     {
-        // 불빛이 있을 때 멈추기
-        return true;
-    }*/
+        isInLight = inLight;
+
+        if (anim != null)
+        {
+            anim.speed = isInLight ? 0 : 1;
+        }
+    }
 }
