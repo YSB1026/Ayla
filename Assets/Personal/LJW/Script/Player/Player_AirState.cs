4e49a43b5ae5a6e3f836b2d0f591b6b8ee5855da
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-05-02 18:03:35 +0900

    Respawn

--- Assets/Personal/LJW/Script/Player/Player_AirState.cs
+++ Assets/Personal/LJW/Script/Player/Player_AirState.cs
@@ -5,9 +5,7 @@ public class Player_AirState : PlayerState
 	private Vector2 airStartPos;
 	private Vector2 airEndPos;
 	private float fallingHeight;
-	private const float DEAD_HEIGHT = 15f;
-
-	private bool isDead = false;//없을시 Exit가 재귀함수가 되므로 스택오버플러우 일어남
+	private const float DEAD_HEIGHT = 8f;
 
 	public Player_AirState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) 
 		: base(_player, _stateMachine, _animBoolName)
@@ -18,7 +16,6 @@ public class Player_AirState : PlayerState
 	{
 		base.Enter();
 		airStartPos = player.transform.position;
-		isDead = false;
 	}
 
 	public override void Update()
@@ -29,20 +26,23 @@ public class Player_AirState : PlayerState
 			player.SetVelocity(player.moveSpeed * 0.8f * xInput, rb.linearVelocityY);
 
 		if (player.IsGroundDetected())
-			stateMachine.ChangeState(player.inputState);
+		{
+			airEndPos = player.transform.position;
+
+			fallingHeight = airStartPos.y - airEndPos.y;
+
+			if (fallingHeight > DEAD_HEIGHT)
+			{
+				stateMachine.ChangeState(player.deadState);
+			}
+			else
+				stateMachine.ChangeState(player.inputState);
+		}
 	}
 
 	public override void Exit()
 	{
 		base.Exit();
-		airEndPos = player.transform.position;
 
-		fallingHeight = airStartPos.y - airEndPos.y;
-
-		if (fallingHeight > DEAD_HEIGHT && !isDead)
-		{
-			isDead = true;
-			stateMachine.ChangeState(player.deadState);
-		}
 	}
 }
