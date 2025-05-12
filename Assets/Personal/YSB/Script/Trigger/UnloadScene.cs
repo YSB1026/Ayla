0d058c8302370f963811ab78dbfde8a44d937b0c
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-05-12 09:23:00 +0900

    25/05/12 커밋

--- Assets/Personal/YSB/Script/Trigger/UnloadScene.cs
+++ Assets/Personal/YSB/Script/Trigger/UnloadScene.cs
@@ -19,7 +19,8 @@ public class UnloadScene : BaseTrigger //클래스 이름 바꾸기
     
     public void LoadEndingCredit()
     {
-        CustomSceneManager.Instance.LoadScene("EndingCredit");
+        Debug.Log("엔딩크레딧");
+        CustomSceneManager.Instance.LoadScene("EndingCreditScene");
     }
 
     protected override void OnPlayerEnter()//얘가 왜 필요햇는지 기억이안나요..
